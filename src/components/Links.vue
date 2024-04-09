57410210e66bb5892636f984f75f10af285e0f8f
lalala666plus <128167824+lalala666plus@users.noreply.github.com>
2024-04-09 20:10:22 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, Cloud, CloudMonitoring, DualScreenStatusBar24Regular, SubVolume, Dragon, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -65,10 +65,10 @@ const siteLinksList = computed(() => {
 const siteIcon = {
   Blog,
   Cloud,
-  CompactDisc,
-  Compass,
-  Book,
-  Fire,
+  CloudMonitoring,
+  DualScreenStatusBar24Regular,
+  SubVolume,
+  Dragon,
   LaptopCode,
 };
 
